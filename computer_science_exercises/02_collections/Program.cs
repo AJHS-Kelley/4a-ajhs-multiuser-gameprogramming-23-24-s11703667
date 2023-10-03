ad1d7008fa52e377e2683910c4195496f905d580
s11703667 <142252946+s11703667@users.noreply.github.com>
2023-10-03 14:23:11 -0400

    Create Program.cs

---
+++ computer_science_exercises/02_collections/Program.cs
@@ -0,0 +1,13 @@
+// 02_Collections, London Baldwin, 10-03-23
+using System;
+
+namespace _02_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
