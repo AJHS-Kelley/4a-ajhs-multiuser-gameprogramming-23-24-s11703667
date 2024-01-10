f8f426a6d4913cd9b14b470bd516568726ae46d2
s11703667 <142252946+s11703667@users.noreply.github.com>
2024-01-10 14:47:09 -0500

    Update programTemplate.cs

--- 06_review/programTemplate.cs
+++ 06_review/programTemplate.cs
@@ -9,7 +9,8 @@ namespace ReviewProject
         {
 
             // Declaring and Assigning Variables
-            // Interger
+            // Interger -- -2,147,483,648 to 2,147,483,647
+            // Interger = 4 Bytes
             int myInt = 5;
 
             // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
@@ -17,25 +18,42 @@ namespace ReviewProject
             Long myLong = 1928742747242904;
 
             // Float
-            // Float = 4 bytes
+            // Float = 4 Bytes
             float myFloat = 3.5F;
             // Double -- Stores up to 15 decimal digits.
-            // Double = 8 bytes
+            // Double = 8 Bytes
             double myDouble = 9.123456789102345D;
             
             // Boolean -- true / false
-            // Boolean = 1 bit
+            // Boolean = 1 Bit
             bool myBool = false;
 
             // String
-            // 2-bytes PER CHARACTER 
+            // 2-Bytes PER CHARACTER 
             // must be in DOUBLE QUOTES " "
             string myString = "i love waffles!";
 
             // Character (char)
             // Single character, in SINGLE QUOTES ' '
-            // 2-bytes
+            // 2-Bytes
             char myChar = 'L';
+
+            // Type Casting -- changing one data type to another
+            // IMPLICIT -- happens automatically when going smaller -> larger
+            // char -> int -> long -> float -> double
+
+             int myInt = 9;
+            double myDouble = myInt;
+            Console.WriteLine(myDouble);
+
+            // EXPLICIT -- Must be done manually when goingh larger -> smaller.
+            // double -> float -> long -> int -> char
+            double exampleDouble = 9.3245678901D;
+            int myInt = (int) myDouble;
+
+            // Conversion Methods
+            Convert.ToString(myInt)
+            Convert.ToDoouble()           
         }
     }
 }
