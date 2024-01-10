710607f607e8efea65c6c5db9c00b55259f01c9a
s11703667 <142252946+s11703667@users.noreply.github.com>
2024-01-10 14:04:49 -0500

    Create Review

---
+++ 06_review/programTemplate.cs
@@ -0,0 +1,50 @@
+// London Baldwin, Program Template, v0.01
+using System;
+
+namespace ReviewProject
+{
+    class ReviewProject
+    {
+        static void Main(string[] args)
+        {
+
+            // Declaring and Assigning Variables
+            // Interger
+            int myInt = 5;
+
+            // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            // Long 8 Bytes
+            Long myLong = 1928742747242904;
+
+            // Float
+            // Float = 4 bytes
+            float myFloat = 3.5F;
+            // Double -- Stores up to 15 decimal digits.
+            // Double = 8 bytes
+            double myDouble = 9.123456789102345D;
+            
+            // Boolean -- true / false
+            // Boolean = 1 bit
+            bool myBool = false;
+
+            // String
+            // 2-bytes PER CHARACTER 
+            // must be in DOUBLE QUOTES " "
+            string myString = "i love waffles!";
+
+            // Character (char)
+            // Single character, in SINGLE QUOTES ' '
+            // 2-bytes
+            char myChar = 'L';
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
